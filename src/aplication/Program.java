a3f32ce25cc179d7169061d7a44a2adf46d157a7
Elizandra Fernandes <luis.f@academico.ifs.edu.br>
2026-02-06 03:16:26 -0300

    Classes Departament e Seller Criadas

---
+++ src/aplication/Program.java
@@ -0,0 +1,11 @@
+package aplication;
+
+import model.entities.Departament;
+import model.entities.Seller;
+
+public class Program {
+    public static void main(String[] args) {
+        Departament departament = new Departament(20, "Departamento");
+        System.out.println(departament);
+    }
+}
---
+++ src/model/entities/Departament.java
@@ -0,0 +1,50 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Departament implements Serializable {
+    private int id;
+    private String nome;
+
+    public Departament(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Departament that = (Departament) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Departament{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+}
